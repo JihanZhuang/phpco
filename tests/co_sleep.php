4dd0b7792fb81ac8d7cdad58e5ea1eb9fdf38a7f
JihanZhuang <869794967@qq.com>
2018-03-15 11:31:44 +0800

    test sleep feature

--- tests/co_sleep.php
+++ tests/co_sleep.php
@@ -1,8 +1,12 @@
 <?php
+function test_sleep(){
+    co::sleep(1);
+}
 co::create(function(){
     for(;;){
     var_dump(123);
-    co::sleep(1);
+    //co::sleep(1);
+    test_sleep();
     var_dump(110);
     }
 });
